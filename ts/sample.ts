0ee59be05e6b1deaac10581a6877a23256759672
Curt Cox <curtcox@gmail.com>
2024-07-24 17:22:54 -0500

    Update sample.ts

--- ts/sample.ts
+++ ts/sample.ts
@@ -1,1 +1,1 @@
-export const exported_constant = 'exported from https://github.com/curtcox/TypescriptSamples/edit/main/ts/sample.ts';
+export const exported_constant = `exported from https://github.com/curtcox/TypescriptSamples/edit/main/ts/sample.ts`;
